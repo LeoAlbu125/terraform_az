abb96a9c52a6ee6c7bfe99afe7dd28814ae1e77a
Leonardo Abreu <leonardoaall125@gmail.com>
2024-11-11 20:54:25 -0300

    terraform state on blob

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "test"      
+    storage_account_name  = "bucketazulzure"
+    container_name        = "terraform-container"          
+    key                   = "terraform.tfstate"  
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
