c9d42367c357a837551c61604714c8caa1d76d0f
Leonardo Abreu <leonardoaall125@gmail.com>
2024-11-11 21:26:55 -0300

    new_changes

--- main.tf
+++ main.tf
@@ -4,14 +4,38 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "test"      
+    resource_group_name   = "test"
     storage_account_name  = "bucketazulzure"
-    container_name        = "terraform-container"          
-    key                   = "terraform.tfstate"  
+    container_name        = "terraform-container"
+    key                   = "terraform.tfstate"
   }
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
+}
+
+# Azure Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "examplestorageacct"
+  resource_group_name       = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier               = "Standard"
+  account_replication_type = "LRS"
+}
+
+# Azure Blob Storage Container
+resource "azurerm_storage_container" "example" {
+  name                    = "example-container"
+  storage_account_name    = azurerm_storage_account.example.name
+}
+
+# Create a virtual directory called "results" (simulated using blob naming convention)
+resource "azurerm_storage_blob" "results_directory" {
+  name                    = "results/.dummy"  # This creates the 'results' folder structure.
+  storage_account_name    = azurerm_storage_account.example.name
+  storage_container_name  = azurerm_storage_container.example.name
+  type                    = "Block"
+  content                 = ""  # This can be empty; it only needs to exist to simulate the directory.
 }
